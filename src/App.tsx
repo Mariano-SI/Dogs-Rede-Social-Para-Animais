d892216084335c7a172767bb1a72e582f5acf308
Mariano Silva <mariano.silva@dtidigital.com>
2023-08-10 21:19:18 -0300

    initial routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import "./App.css"
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+import Home from './Pages/Home/Home'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './Pages/Login/Login'
 
 function App() {
   
 
   return (
-    <>
-    olá, mundo!
-    </>
+    <div>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </div>
   )
 }
 
