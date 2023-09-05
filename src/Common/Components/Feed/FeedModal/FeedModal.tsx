472dc557f847cde10d3d220998cb7d4dfdc29a01
Mariano Silva <mariano.silva@dtidigital.com>
2023-09-04 23:40:05 -0300

    novos componnets para PhotoModal

--- src/Common/Components/Feed/FeedModal/FeedModal.tsx
+++ src/Common/Components/Feed/FeedModal/FeedModal.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import IPhoto from '../../../interfaces/IPhoto'
+import { FeedModalStyled } from './FeedModal.style'
+import useFetch from '../../../CustomHooks/useFEtch'
+import { GET_PHOTO } from '../../../api'
+import Error from '../../Error/Error'
+import Loading from '../../Loading/Loading'
+import PhotoContent from '../../../../Features/Photo/Components/PhotoContent/PhotoContent'
 
+interface IFeedModal{
+  photo?: IPhoto;
+}
 
-const FeedModal = () => {
+const FeedModal = ({photo}: IFeedModal) => {
+  const {data, error, loading, request} = useFetch();
+
+  useEffect(()=>{
+    if(photo && photo.id){
+      const {url, options} = GET_PHOTO(photo.id);
+      request(url, options);
+    }
+  },[photo, request])
+
   return (
-    <div>FeedModal</div>
+    <FeedModalStyled>
+      {error && <Error message={error}/>}
+      {loading && <Loading/>}
+      {data ? <PhotoContent data={data}/>: ''}
+    </FeedModalStyled>
   )
 }
 
