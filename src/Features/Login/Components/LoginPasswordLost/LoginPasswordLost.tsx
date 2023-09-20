468406ad900ad5be8249d2b73decbd44827a77b6
Mariano Silva <mariano.silva@ufvjm.edu.br>
2023-09-20 17:12:29 -0300

    passaword lost ok

--- src/Features/Login/Components/LoginPasswordLost/LoginPasswordLost.tsx
+++ src/Features/Login/Components/LoginPasswordLost/LoginPasswordLost.tsx
@@ -1,10 +1,45 @@
 import React from 'react'
+import { LoginPasswordLostStyled } from './LoginPasswordLost.style'
+import Input from '../../../../Common/Components/Forms/Input/Input'
+import Button from '../../../../Common/Components/Forms/Button/Button'
+import useForm from '../../../../Common/CustomHooks/useForm'
+import useFetch from '../../../../Common/CustomHooks/useFEtch'
+import { PASSWORD_LOST } from '../../../../Common/api'
+import Error from '../../../../Common/Components/Error/Error'
 
-type Props = {}
+function LoginPasswordLost():JSX.Element{
 
-function LoginPasswordLost(props: Props):JSX.Element{
+  const login = useForm();
+
+  const {data, loading, error, request} = useFetch();
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement> ){
+    event.preventDefault();
+
+    if(login.validate()){
+
+      const {url, options} = PASSWORD_LOST({login: login.value, url: window.location.href.replace('perdeu', 'resetar')})
+
+      const {json} = await request(url, options);
+      console.log(json)
+    }
+
+  }
+
   return (
-    <div>LoginPasswordLost</div>
+    <LoginPasswordLostStyled>
+      <h1 className='title'>Perdeu a senha?</h1>
+      {data ? (
+        <p style={{color:"#4C1"}}>{data}</p> 
+        ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label='Email/ Usuário' type='text' name='login' {...login}/>
+          {loading? <Button disabled>Enviando...</Button>:<Button>Enviar Email</Button>}
+          
+        </form>
+        )}     
+      <Error message={error}/>
+    </LoginPasswordLostStyled>
   )
 }
 
