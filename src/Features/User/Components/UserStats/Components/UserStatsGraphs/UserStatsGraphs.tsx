976825adac92eb8e192e685f77636c7586f20c6e
Mariano Silva <mariano.silva@ufvjm.edu.br>
2023-09-21 17:14:14 -0300

    stats page: graph lib implemented

---
+++ src/Features/User/Components/UserStats/Components/UserStatsGraphs/UserStatsGraphs.tsx
@@ -0,0 +1,66 @@
+import React, {useEffect, useState} from 'react'
+import { UserStatsGraphsStyled } from './UseStatsGraphs.style';
+import {VictoryPie, VictoryChart, VictoryBar} from 'victory'
+
+
+interface IData{
+    id:number;
+    title:string;
+    acessos: string;
+}
+
+interface IUserStatsGraphs{
+    data: Array<IData>
+}
+interface IPieGraph{
+    x: string;
+    y:number;
+}
+
+const UserStatsGraphs = ({data}: IUserStatsGraphs) => {
+    const [graphData, setGraphData] = useState<[]|Array<IPieGraph>>([]);
+
+    const [total, setTotal] = useState(0);
+
+    useEffect(()=>{
+        const graph = data.map((item)=>(
+            {x:item.title, y:+item.acessos}
+        ));
+            console.log(graph)
+        setGraphData(graph);
+
+        setTotal(data.map(({acessos})=> +acessos).reduce((a,b)=> a+b, 0));
+    }, [data]);
+
+    return (
+        <UserStatsGraphsStyled className="animeLeft">
+            <div className='total graphItem'>
+                <p>Acessos: {total}</p>
+            </div>
+            <div className='graphItem'>
+                <VictoryPie 
+                data={graphData} 
+                innerRadius={50} 
+                padding={{top:20, bottom:20, left:80, right:80 }} 
+                style={{
+                    data:{
+                        fillOpacity:.9,
+                        stroke:'#fff',
+                        strokeWidth:2
+                    }, labels:{
+                        fontSize:14,
+                        fill:'#333',
+                    }}
+                    }/>
+            </div>
+            <div className='graphItem'>
+                <VictoryChart>
+                    <VictoryBar alignment='start' data={graphData}>
+                    </VictoryBar>
+                </VictoryChart>
+            </div>
+        </UserStatsGraphsStyled>
+    )
+}
+
+export default UserStatsGraphs
--- src/Features/User/Components/UserStats/UserStats.tsx
+++ src/Features/User/Components/UserStats/UserStats.tsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, {useEffect}from 'react'
 import Head from '../../../../Common/Components/Head/Head'
+import useFetch from '../../../../Common/CustomHooks/useFEtch'
+import { GET_STATS } from '../../../../Common/api'
+import Loading from '../../../../Common/Components/Loading/Loading'
+import Error from '../../../../Common/Components/Error/Error'
+import UserStatsGraphs from './Components/UserStatsGraphs/UserStatsGraphs'
 
-type Props = {}
 
-const UserStats = (props: Props) => {
-  return (
-    <div>
-      <Head title='Estatísticas' />
-    </div>
-  )
+
+const UserStats = () => {
+
+  const {data, error, loading, request} = useFetch();
+
+  useEffect(()=>{
+    async function getData(){
+      const {url, options} = GET_STATS();
+      await request(url, options);
+    }
+
+    getData();
+  },[request]);
+
+  if(loading) return <Loading/>
+  if(error) return <Error message={error}/>
+  if(data){
+    return (
+      <div>
+        <Head title='Estatísticas' />
+        <UserStatsGraphs data={data}/>
+      </div>
+    )
+  }
+  else return null;
 }
 
 export default UserStats
